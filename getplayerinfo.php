badc64e4fff7006d72a2fbe0cc20dc077e763ca1
Alex Law <>
2018-03-26 10:35:10 -0700

    added goalie query

--- getplayerinfo.php
+++ getplayerinfo.php
@@ -2,54 +2,90 @@
 // Get a connection for the database
 require_once('../mysqli_connect.php');
 
-// Create a query for the database
-$query = "SELECT TOP 10 name, year, PIM, points, assists, goals, SOG, plusminus
+// Create queries for the database
+$skaterquery = "SELECT TOP 10 name, year, PIM, points, assists, goals, SOG, plusminus
           FROM skater_statistic
           ORDER BY points";
-
+$goaliequery = "SELECT TOP 5 name, year, win, loss, tie, GAA, saving_percent, SO
+                FROM goalie_statistic
+                ORDER BY win";
 // Get a response from the database by sending the connection
 // and the query
-$response = @mysqli_query($dbc, $query);
+$skaterresponse = @mysqli_query($dbc, $skaterquery);
+$goalieresponse = @mysqli_query($dbc, $goaliequery);
 
 // If the query executed properly proceed
-if($response){
+if($skaterresponse){
+  echo '<table align="left"
+  cellspacing="5" cellpadding="8">
 
-echo '<table align="left"
-cellspacing="5" cellpadding="8">
+  <tr>
+  	<td align="left"><b>Name</b></td>
+    <td align="left"><b>Year</b></td>
+    <td align="left"><b>Goals</b></td>
+    <td align="left"><b>Assists</b></td>
+    <td align="left"><b>Points</b></td>
+    <td align="left"><b>SOG</b></td>
+    <td align="left"><b>PIM</b></td>
+    <td align="left"><b>+/-</b></td>
 
-<tr>
-	<td align="left"><b>Name</b></td>
-  <td align="left"><b>Year</b></td>
-  <td align="left"><b>Goals</b></td>
-  <td align="left"><b>Assists</b></td>
-  <td align="left"><b>Points</b></td>
-  <td align="left"><b>SOG</b></td>
-  <td align="left"><b>PIM</b></td>
-  <td align="left"><b>+/-</b></td>
-
-</tr>';
+  </tr>';
 
 // mysqli_fetch_array will return a row of data from the query
 // until no further data is available
-while($row = mysqli_fetch_array($response)){
+  while($row = mysqli_fetch_array($skaterresponse)){
 
-echo '<tr><td align="left">' .$row['name'] . 
-    '</td><td align="left">' .$row['year'] .
-    '</td><td align="left">' .$row['goals'] .
-    '</td><td align="left">' .$row['assists'] .
-    '</td><td align="left">' .$row['points'] .
-    '</td><td align="left">' .$row['SOG'] .
-    '</td><td align="left">' .$row['PIM'] .
-    '</td><td align="left">' .$row['plusminus'] .
-    '</td>';
+  echo '<tr><td align="left">' .$row['name'] .
+      '</td><td align="left">' .$row['year'] .
+      '</td><td align="left">' .$row['goals'] .
+      '</td><td align="left">' .$row['assists'] .
+      '</td><td align="left">' .$row['points'] .
+      '</td><td align="left">' .$row['SOG'] .
+      '</td><td align="left">' .$row['PIM'] .
+      '</td><td align="left">' .$row['plusminus'] .
+      '</td>';
 
-echo '</tr>';
+  echo '</tr>';
+  }
+  echo '</table>';
+  } else {
+    echo "Couldn't issue database query<br />";
+    echo mysqli_error($dbc);
 }
-echo '</table>';
-} else {
-echo "Couldn't issue database query<br />";
 
-echo mysqli_error($dbc);
+if ($goaliequery) {
+  echo '<table align="left"
+  cellspacing="5" cellpadding="8">
+
+  <tr>
+  	<td align="left"><b>Name</b></td>
+    <td align="left"><b>Year</b></td>
+    <td align="left"><b>Win</b></td>
+    <td align="left"><b>Loss</b></td>
+    <td align="left"><b>Tie</b></td>
+    <td align="left"><b>GAA</b></td>
+    <td align="left"><b>Sv%</b></td>
+    <td align="left"><b>SO</b></td>
+  </tr>';
+
+  while($row = mysqli_fetch_array($goalieresponse)) {
+    echo '<tr><td align="left">' .$row['name'] .
+        '</td><td align="left">' .$row['year'] .
+        '</td><td align="left">' .$row['win'] .
+        '</td><td align="left">' .$row['loss'] .
+        '</td><td align="left">' .$row['tie'] .
+        '</td><td align="left">' .$row['gaa'] .
+        '</td><td align="left">' .$row['saving_percent'] .
+        '</td><td align="left">' .$row['so'] .
+        '</td>';
+
+    echo '</tr>';
+    }
+    echo '</table>';
+    } else {
+      echo "Couldn't issue database query<br />";
+      echo mysqli_error($dbc);
+  }
 }
 
 // Close connection to the database
