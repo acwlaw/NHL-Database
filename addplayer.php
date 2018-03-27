761f60cc91e40ee6e4ca6e0199b92ad62affb0cd
Dylan Wang <a5w0b@ugrad.cs.ubc.ca>
2018-03-27 01:23:46 -0700

    addplayer.php now directs to itself upon submit, no need for playeradded.php

--- addplayer.php
+++ addplayer.php
@@ -3,7 +3,66 @@
 <title>Add Player</title>
 </head>
 <body>
-<form action="playeradded.php" method="post">
+
+<?php
+
+if(isset($_POST['submit'])){
+    $data = array("name", "position", "type", "team", "year");
+    $data_missing = array();
+    foreach($_POST as $key => $value) {
+        if(empty($value)) {
+            $data_missing[] = $key;
+        }
+        $$key = $value;
+    }
+    // $name, $position, $type, $team, $year
+
+    if(empty($data_missing)){
+        require_once('../../mysqli_connect.php');
+        if(mysqli_fetch_array(mysqli_query($dbc, "SELECT name
+            FROM players
+            WHERE name = '" . $name . "'"))['name'] == $name) {
+            // Player already exists
+            echo 'Player with this name already exists';
+        } else {
+            // Player does not exist
+            // Add player to players
+            $query = "INSERT INTO players VALUES (?, ?)";
+            $stmt = mysqli_prepare($dbc, $query);
+            mysqli_stmt_bind_param($stmt, "ss", $name, $position);
+            mysqli_stmt_execute($stmt);
+            $affected_rows = mysqli_stmt_affected_rows($stmt);
+            
+            // Add player to specific type
+            $stmt = mysqli_prepare($dbc, "INSERT INTO " . $type . " VALUES (?, ?)");
+            if ($type == "active" || $type == "free") {
+                mysqli_stmt_bind_param($stmt, "ss", $name, $team);
+            } else { // retired
+                mysqli_stmt_bind_param($stmt, "si", $name, $year);
+            }
+            mysqli_stmt_execute($stmt);
+            $affected_rows += mysqli_stmt_affected_rows($stmt);
+
+            if ($affected_rows == 2){
+                echo 'Player Entered';
+            } else {
+                echo 'Unknown Error Occurred<br />';
+                //echo mysqli_error();
+            }
+            //mysqli_stmt_close($stmt);
+            //mysqli_close($dbc);
+        }
+    } else {
+        echo 'You need to enter the following data<br />';
+        foreach($data_missing as $missing){
+            echo "$missing<br />";
+        }
+    }
+}
+
+?>
+
+<form action="addplayer.php" method="post">
 
 <b>Add a New Player</b><br>
   Name:<br>
@@ -12,54 +71,45 @@
   <input type="text" name="position"><br>
   <input type="radio" name="type" value="active" onclick="showTeam()" checked>Active Player<br>
   <input type="radio" name="type" value="retired" onclick="showYear()">Retired player<br>
-  <input type="radio" name="type" value="free" onclick="showTeam()">Free Agent<br>
+  <input type="radio" name="type" value="free_agent" onclick="showTeam()">Free Agent<br>
   
   <div id="team">
-  Team:<br>
+  Team / Last Team:<br>
   <select name="team">
-  <?php
-        require_once('../../mysqli_connect.php');
-  $query = "SELECT city, team_name
-          FROM team
-          ORDER BY city";
-$response = @mysqli_query($dbc, $query);
-if($response) {
-  while($row = mysqli_fetch_array($response)) {
-    echo "<option>" . $row['team_name'] . "</option>";
-  }
-}
-?>
-    </select>
+    <?php
+    require_once('../../mysqli_connect.php');
+    $query = "SELECT city, team_name
+        FROM team
+        ORDER BY city";
+    $response = mysqli_query($dbc, $query);
+    if($response) {
+      while($row = mysqli_fetch_array($response)) {
+        echo "<option>" . $row['team_name'] . "</option>";
+      }
+    }
+    ?><br>
+  </select>
   </div>
-
-
-
-
 
   <div id="year" style="display:none">
   Retirement Year:<br>
   <select name="year">
-    <option selected>1917</option>
-    <?php for ($i = 1918; $i <= 2018; $i++) {echo '<option>', $i, '</option>';} ?>
+    <?php for ($i = 1917; $i <= 2018; $i++) {echo '<option>', $i, '</option>';} ?>
   </select><br>
   </div>
   
   <input type="submit" name="submit" value="Add Player">
 </form>
-
-
-
   
   <script>
     function showTeam() {
-        document.getElementById("team").style.display = "inline";
+        document.getElementById("team").style.display = "";
         document.getElementById("year").style.display = "none";
     }
     function showYear() {
-        document.getElementById("year").style.display = "inline";
+        document.getElementById("year").style.display = "";
         document.getElementById("team").style.display = "none";
     }
-  </script>
   </script>
 </body>
 </html>
